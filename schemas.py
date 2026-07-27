e566d70154734f26e816cbd8c4cfe142f4ac8ac6
Luiz Fernando <luizfccastilho@gmail.com>
2026-07-27 11:57:11 -0300

    Cria schemas Pydantic para validação de entrada

---
+++ schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+# O que a API recebe. O que é obrigatório ou não na validação da entrada
+# status: str = "em andamento" mostra que status tem um valor obrigatório igual em models.py, caso o usuário tente criar um experimento sem mandar o campo status o Pydantic já preenche com "em andamento" 
+class ExperimentCreate(BaseModel):
+    title: str
+    description: str
+    responsible_researcher: str
+    status: str = "em andamento" 
+    
+    
+# O que a API retorna: ExperimentOut. O que a API retorna quando alguém busca ou cria um experimento
+class ExperimentOut(BaseModel):
+    id: int
+    title: str
+    description: str
+    responsible_researcher: str
+    status: str
+    created_time: datetime
+    
+    
